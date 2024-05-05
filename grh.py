b223fa9e2595bb14f9575621493d9b5b0eb1d176
cpbeto <41555339+cpbeto@users.noreply.github.com>
2024-05-05 12:27:45 -0300

    Refactor loading graphic indexes

--- grh.py
+++ grh.py
@@ -19,11 +19,25 @@ def load():
         data = f.read(grh_header_t.size)
         grh_count = grh_header_t.unpack_from(data)[1]
 
+        grh_data = [None] * (grh_count + 1) # Workaround VB6 1-based indexes
 
-        grh_data = {} # Workaround VB6 1-based indexes, also allows random access
+        for _ in range(grh_count):
+            # typedef struct {
+            #     int16_t sx;
+            #     int16_t sy;
+            #     uint32_t filenum;
+            #     int16_t pixel_width;
+            #     int16_t pixel_height;
+            #     float tile_width;
+            #     float tile_height;
+            #     int16_t num_frames;
+            #     uint32_t *frames;
+            #     float speed;
+            # } grh_t;
 
-        try:
-            for i in range(grh_count):
+            grh = {}
+
+            try:
                 grh_index_t = Struct('<I')
                 data = f.read(grh_index_t.size)
                 grh_index = grh_index_t.unpack_from(data)[0]
@@ -33,23 +47,8 @@ def load():
                 num_frames = num_frames_t.unpack_from(data)[0]
 
                 assert num_frames > 0
-
-
-                # typedef struct {
-                #     int16_t sx;
-                #     int16_t sy;
-                #     uint32_t filenum;
-                #     int16_t pixel_width;
-                #     int16_t pixel_height;
-                #     float tile_width;
-                #     float tile_height;
-                #     int16_t num_frames;
-                #     uint32_t *frames;
-                #     float speed;
-                # } grh_t;
-
-                grh = {}
-                grh['frames'] = {} # Workaround VB6 1-based indexes
+                grh['num_frames'] = num_frames
+                grh['frames'] = []
 
                 if (num_frames == 1):
                     grh_t = Struct('<Ihhhh')
@@ -60,13 +59,12 @@ def load():
                     grh['tile_width'] = grh['pixel_width'] / TILE_PIXEL_WIDTH
                     grh['tile_height'] = grh['pixel_height'] / TILE_PIXEL_HEIGHT
 
-                    grh['frames'][1] = grh_index
+                    grh['frames'].append(grh_index)
                 else:
-                    for frame_index in range(1, num_frames+1):
+                    for _ in range(num_frames):
                         frame_t = Struct('<I')
                         frame = frame_t.unpack_from(f.read(frame_t.size))[0]
-
-                        grh['frames'][frame_index] = frame
+                        grh['frames'].append(frame)
 
                     speed_t = Struct('<f')
                     speed = speed_t.unpack_from(f.read(speed_t.size))[0]
@@ -80,7 +78,7 @@ def load():
 
                 grh_data[grh_index] = grh
                 
-        except Exception:
-            pass
+            except Exception:
+                pass
 
     return grh_data
