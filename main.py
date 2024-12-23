ecc9749c41268d12e7d1f5e0fe8ad6c87ec98a8c
cpbeto <41555339+cpbeto@users.noreply.github.com>
2024-12-22 22:08:44 -0300

    Implement world position and map changes
    Breaks rendering of layers 1-3, needs fix.

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import json
+import sys
+
 from ursina import *
 import PIL.Image
 
@@ -27,69 +30,81 @@ camera_height = ceil(window_height / PIXELS_PER_TILE)
 Sprite.ppu = PIXELS_PER_TILE
 Texture.default_filtering = None
 
-map = map.load(1)
-tiles = map['tiles']
-
+map_pool = {}
 grh_data = grh.load()
 
 
 layer_enabled = [True] * 4
 need_to_render = True
-position = Vec3(50, 50, 0)
+position = Vec4(50, 50, 0, 1) # x, y, z-index, map-index
 def input(key):
     global layer_enabled, need_to_render, position
 
     if key in 'wasd0123':
         need_to_render = True
 
-    if key == 'w':
-        position -= (0,1,0)
-    elif key == 's':
-        position += (0,1,0)
-    elif key == 'd':
-        position += (1,0,0)
-    elif key == 'a':
-        position -= (1,0,0)
-    elif key == '0':
-        layer_enabled[0] = not layer_enabled[0]
-    elif key == '1':
-        layer_enabled[1] = not layer_enabled[1]
-    elif key == '2':
-        layer_enabled[2] = not layer_enabled[2]
-    elif key == '3':
-        layer_enabled[3] = not layer_enabled[3]
+    # TODO: Abstract into Camera, Player or Position class
+    if key in 'wasd':
+        if key == 'w':
+            position += (0,-1)
+        elif key == 's':
+            position += (0,1)
+        elif key == 'd':
+            position += (1,0)
+        elif key == 'a':
+            position += (-1,0)
+
+        map = map_pool[position.w]
+        tile = map['tiles'][int(position.x) + 100 * int(position.y)]
+
+        # Change map
+        if tile['exit']:
+            position = Vec4(tile['exit']['x'], tile['exit']['y'], position.z, tile['exit']['map'])
+    else:
+        if key == '0':
+            layer_enabled[0] = not layer_enabled[0]
+        elif key == '1':
+            layer_enabled[1] = not layer_enabled[1]
+        elif key == '2':
+            layer_enabled[2] = not layer_enabled[2]
+        elif key == '3':
+            layer_enabled[3] = not layer_enabled[3]
 
 
 texturePool = TexturePool()
 sprite_pool = {}
-def render(x, y):
+def render(camera_position: Vec4):
     global layer_enabled
-
-    x, y = int(x), int(y)
+    camera_position = round(camera_position)
 
     for sprite in sprite_pool.values():
         sprite.disable()
 
-    for k in range(4):
-        if not layer_enabled[k]:
+    for z in range(4):
+        if not layer_enabled[z]:
             continue
         
         for j in range(-camera_height//2, camera_height//2 + 1):
             for i in range(-camera_width//2, camera_width//2 + 1):
-                map_position = Vec3(x + i, y - j, k)
+                world_position = camera_position + (i,-j)
 
                 # Camera out of bounds
                 # TODO: Render adyacent map?
-                if map_position.x < 0 or map_position.x >= 100:
+                if world_position.x < 0 or world_position.x >= 100:
                     continue
-                if map_position.y < 0 or map_position.y >= 100:
+                if world_position.y < 0 or world_position.y >= 100:
                     continue
 
-                # TODO: Clean up these indices
-                tile = tiles[int(map_position.x) + 100 * int(map_position.y)]
+                # TODO: Abstract into MapPool class
+                if world_position.w not in map_pool:
+                    map_pool[world_position.w] = map.load(int(world_position.w))
+                tiles = map_pool[world_position.w]['tiles']
 
-                # TODO: Re-define these data structures so access is straightforward
-                grh_index = tile['grh'][k]
+                # TODO: Improve tile indexing
+                tile = tiles[int(world_position.x) + 100 * int(world_position.y)]
+
+                # TODO: Redefine these data structures so access is straightforward
+                grh_index = tile['grh'][z]
                 if not grh_index:
                     continue
                 grh = grh_data[grh_index]
@@ -102,23 +117,24 @@ def render(x, y):
                 texture = texturePool[filename]
                 width, height = texture.size
 
-                sprite_position = Vec3(i, j, -k)
-                if k in [1, 2, 3]:
-                    sprite_position.y += (grh['pixel_height']/PIXELS_PER_TILE - 1) / 2
+                screen_position = Vec3(i, j, -z)
+                # Ad hoc offset found in legacy code
+                if z in [1, 2, 3]:
+                    screen_position.y += (grh['pixel_height']/PIXELS_PER_TILE - 1) / 2
 
                 # TODO: Clean this up, refactor sprite pool
-                if map_position not in sprite_pool:
+                if world_position not in sprite_pool:
                     s = Sprite(
                         texture,
-                        position=sprite_position,
+                        position=screen_position,
                         texture_scale=(grh['pixel_width']/width, grh['pixel_height']/height),
                         texture_offset=(grh['sx']/width, height - (grh['sy']/height + grh['pixel_height']/height))
                     )
                     s.scale = (grh['pixel_width']/PIXELS_PER_TILE, grh['pixel_height']/PIXELS_PER_TILE, 1)
-                    sprite_pool[map_position] = s
+                    sprite_pool[world_position] = s
                 else:
-                    sprite_pool[map_position].set_position(sprite_position)
-                    sprite_pool[map_position].enable()
+                    sprite_pool[world_position].set_position(screen_position)
+                    sprite_pool[world_position].enable()
 
 
 def update():
@@ -126,6 +142,6 @@ def update():
 
     if need_to_render:
         need_to_render = False
-        render(position.x, position.y)
+        render(position)
 
 app.run()
