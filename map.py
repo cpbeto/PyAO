4085de536406874aeea3bd3eb1ba7e301c7c9f20
cpbeto <41555339+cpbeto@users.noreply.github.com>
2024-12-22 21:56:31 -0300

    Load map .inf files
    Objects, NPCs and exit tiles (adjacent maps, portals, etc.)

--- map.py
+++ map.py
@@ -7,12 +7,13 @@ GRAPHICS_PATH = ASSETS_PATH + 'graphics/'
 MAPS_PATH = ASSETS_PATH + 'maps/'
 
 MAP_EXTENSION = '.map'
+INF_EXTENSION = '.inf'
 MAP_WIDTH = 100
 MAP_HEIGHT = 100
 
 
 def load(map_number: int):
-    filepath = MAPS_PATH + 'mapa' + str(map_number) + MAP_EXTENSION
+    filepath = MAPS_PATH + 'Mapa' + str(map_number) + MAP_EXTENSION
 
     map = {}
     map['name'] = 'Ullathorpe'
@@ -36,7 +37,7 @@ def load(map_number: int):
         struct_unpack = Struct(map_header_fmt).unpack_from
 
         data = f.read(map_header_size)
-        s = struct_unpack(data)
+        # s = struct_unpack(data)
 
         tiles = []
         for y in range(MAP_HEIGHT):
@@ -79,6 +80,7 @@ def load(map_number: int):
                     trigger = Struct('<H').unpack_from(data)[0]
                     tile['trigger'] = trigger
 
+                tile['exit'] = None
                 tile['npc'] = None
                 tile['object'] = None
 
@@ -87,5 +89,70 @@ def load(map_number: int):
         assert len(tiles) == MAP_WIDTH * MAP_HEIGHT
 
         map['tiles'] = tiles
+
+        load_inf(map_number, map)
+
+    return map
+
+def load_inf(map_number: int, map: dict):
+    filepath = MAPS_PATH + 'Mapa' + str(map_number) + INF_EXTENSION
+
+    with open(filepath, 'rb') as f:
+
+        # typedef struct {
+        #     int16_t padding1;
+        #     int16_t padding2;
+        #     int16_t padding3;
+        #     int16_t padding4;
+        #     int16_t padding5;
+        # } inf_header_t;
+
+        inf_header_fmt = '<hhhhh'
+        inf_header_size = struct.calcsize(inf_header_fmt)
+        struct_unpack = Struct(inf_header_fmt).unpack_from
+
+        data = f.read(inf_header_size)
+        # s = struct_unpack(data)
+
+        for y in range(MAP_HEIGHT):
+            for x in range(MAP_WIDTH):
+                # Read tile flags
+                data = f.read(Struct('<B').size)
+                flags = Struct('<B').unpack_from(data)[0]
+
+                # Exit tile
+                if flags & 1:
+                    exit = {}
+
+                    data = f.read(Struct('<H').size)
+                    exit['map'] = Struct('<H').unpack_from(data)[0]
+
+                    data = f.read(Struct('<H').size)
+                    exit['x'] = Struct('<H').unpack_from(data)[0]
+
+                    data = f.read(Struct('<H').size)
+                    exit['y'] = Struct('<H').unpack_from(data)[0]
+
+                    map['tiles'][x + 100 * y]['exit'] = exit
+
+                # NPC
+                if flags & 2:
+                    data = f.read(Struct('<H').size)
+                    npc = Struct('<H').unpack_from(data)[0]
+
+                    if npc > 0:
+                        map['tiles'][x + 100 * y]['npc'] = npc
+                        # TODO: Create NPC
+                        pass
+
+                # Object
+                if flags & 4:
+                    data = f.read(Struct('<H').size)
+                    obj_index = Struct('<H').unpack_from(data)[0]
+
+                    data = f.read(Struct('<H').size)
+                    obj_amount = Struct('<H').unpack_from(data)[0]
+
+                    map['tiles'][x + 100 * y]['obj'] = {'index': obj_index, 'amount':obj_amount}
 
     return map
