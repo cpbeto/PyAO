40f8912c7c4cc86ddd21f013afa1d253e7107fa8
cpbeto <41555339+cpbeto@users.noreply.github.com>
2024-12-23 21:03:47 -0300

    Render objects

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import PIL.Image
 
 import map
 import grh
+import ini
 from texture import TexturePool
 
 
@@ -32,7 +33,8 @@ Texture.default_filtering = None
 
 map_pool = {}
 grh_data = grh.load()
-
+ini_manager = ini.IniManager()
+ini_manager.load()
 
 layer_enabled = [True] * 4
 need_to_render = True
@@ -82,17 +84,20 @@ def render(camera_position: Vec4):
     if camera_position.w not in map_pool:
         map_pool[camera_position.w] = map.load(int(camera_position.w))
 
+    # TODO: Unify entity pooling
     for sprite in sprite_pool.values():
         sprite.disable()
+    for npc_entity in npc_pool.values():
+        npc_entity.disable()
+    for obj_entity in obj_pool.values():
+        obj_entity.disable()
 
     for z in range(4):
         camera_position.z = z
 
-        if not layer_enabled[z]:
-            continue
-        
         for j in range(-camera_height//2, camera_height//2 + 1):
             for i in range(-camera_width//2, camera_width//2 + 1):
+                screen_position = Vec3(i, j, -z)
                 world_position = camera_position + (i,-j)
 
                 # Camera out of bounds
@@ -107,76 +112,72 @@ def render(camera_position: Vec4):
                 # TODO: Improve tile indexing
                 tile = tiles[int(world_position.x) + 100 * int(world_position.y)]
 
-                # TODO: Redefine these data structures so access is straightforward
-                grh_index = tile['grh'][z]
-                if not grh_index:
-                    continue
-                grh = grh_data[grh_index]
-                
-                if grh['num_frames'] != 1:
-                    continue
+                if layer_enabled[z]:
+                    # TODO: Redefine these data structures so access is straightforward
+                    grh_index = tile['grh'][z]
+                    if not grh_index:
+                        continue
+                    grh = grh_data[grh_index]
 
-                filename = str(grh['filenum'])
+                    if grh['num_frames'] != 1:
+                        continue
 
-                texture = texturePool[filename]
-                width, height = texture.size
+                    filename = str(grh['filenum'])
 
-                screen_position = Vec3(i, j, -z)
-                # Ad hoc offset found in legacy code
-                if z in [1, 2, 3]:
-                    screen_position.y += (grh['pixel_height']/PIXELS_PER_TILE - 1) / 2
+                    texture = texturePool[filename]
+                    width, height = texture.size
 
-                # TODO: Clean this up, refactor sprite pool
-                if world_position not in sprite_pool:
-                    s = Sprite(
-                        texture,
-                        position=screen_position,
-                        texture_scale=(grh['pixel_width']/width, grh['pixel_height']/height),
-                        texture_offset=(grh['sx']/width, height - (grh['sy']/height + grh['pixel_height']/height))
-                    )
-                    s.scale = (grh['pixel_width']/PIXELS_PER_TILE, grh['pixel_height']/PIXELS_PER_TILE, 1)
-                    sprite_pool[world_position] = s
-                else:
-                    sprite_pool[world_position].set_position(screen_position)
-                    sprite_pool[world_position].enable()
+                    # Ad hoc offset found in legacy code
+                    if z in [1, 2, 3]:
+                        screen_position.y += (grh['pixel_height']/PIXELS_PER_TILE - 1) / 2
 
-    for npc_entity in npc_pool.values():
-        npc_entity.disable()
+                    # TODO: Clean this up, refactor sprite pool
+                    if world_position not in sprite_pool:
+                        s = Sprite(
+                            texture,
+                            position=screen_position,
+                            texture_scale=(grh['pixel_width']/width, grh['pixel_height']/height),
+                            texture_offset=(grh['sx']/width, height - (grh['sy']/height + grh['pixel_height']/height))
+                        )
+                        s.scale = (grh['pixel_width']/PIXELS_PER_TILE, grh['pixel_height']/PIXELS_PER_TILE, 1)
+                        sprite_pool[world_position] = s
+                    else:
+                        sprite_pool[world_position].set_position(screen_position)
+                        sprite_pool[world_position].enable()
 
-    for j in range(-camera_height // 2, camera_height // 2 + 1):
-        for i in range(-camera_width // 2, camera_width // 2 + 1):
-            screen_position = Vec3(i, j, 0)
-            world_position = camera_position + (i, -j)
+                if z == 0:
+                    if tile['npc']:
+                        npc_index = tile['npc']
+                        if npc_index not in npc_pool:
+                            npc_entity = Entity(model='cube', color=color.gold, collider='box', position=screen_position, scale=(0.5, 0.5))
+                            npc_pool[npc_index] = npc_entity
+                        npc_pool[npc_index].set_position(screen_position)
+                        npc_pool[npc_index].enable()
 
-            tiles = map_pool[world_position.w]['tiles']
-            tile = tiles[int(world_position.x) + 100 * int(world_position.y)]
-
-            if tile['npc']:
-                npc_index = tile['npc']
-                if npc_index not in npc_pool:
-                    npc_entity = Entity(model='cube', color=color.gold, collider='box', position=screen_position, scale=(0.5, 0.5))
-                    npc_pool[npc_index] = npc_entity
-                npc_pool[npc_index].set_position(screen_position)
-                npc_pool[npc_index].enable()
-
-    for obj_entity in obj_pool.values():
-        obj_entity.disable()
-
-    for j in range(-camera_height // 2, camera_height // 2 + 1):
-        for i in range(-camera_width // 2, camera_width // 2 + 1):
-            screen_position = Vec3(i, j, 0)
-            world_position = camera_position + (i, -j)
-
-            tiles = map_pool[world_position.w]['tiles']
-            tile = tiles[int(world_position.x) + 100 * int(world_position.y)]
-
-            if tile['obj']:
-                obj_index = tile['obj']['index']
-                if obj_index not in obj_pool:
-                    obj_entity = Entity(model='cube', color=color.blue, collider='box', position=screen_position, scale=(0.5, 0.5))
-                    obj_pool[obj_index] = obj_entity
-                obj_pool[obj_index].set_position(screen_position)
-                obj_pool[obj_index].enable()
+                    if tile['obj']:
+                        obj_index = tile['obj']['index']
+                        if world_position not in obj_pool:
+                            # obj_entity = Entity(model='cube', color=color.blue, collider='box', position=screen_position, scale=(0.5, 0.5))
+                            objs = ini_manager['obj.dat']
+                            grh_index = objs[f'OBJ{obj_index}'].getint('GrhIndex')
+                            if not grh_index or grh_data[grh_index]['num_frames'] != 1:
+                                obj_entity = Entity(model='cube', color=color.gray, collider='box', position=screen_position, scale=(0.5, 0.5))
+                            else:
+                                grh = grh_data[grh_index]
+                                filename = str(grh['filenum'])
+                                texture = texturePool[filename]
+                                width, height = texture.size
+                                screen_position.y += (grh['pixel_height'] / PIXELS_PER_TILE - 1) / 2
+                                obj_entity = Sprite(
+                                    texture,
+                                    position=screen_position,
+                                    texture_scale=(grh['pixel_width'] / width, grh['pixel_height'] / height),
+                                    texture_offset=(
+                                    grh['sx'] / width, height - (grh['sy'] / height + grh['pixel_height'] / height))
+                                )
+                            obj_pool[world_position] = obj_entity
+                        obj_pool[world_position].set_position(screen_position)
+                        obj_pool[world_position].enable()
 
 
 def update():
